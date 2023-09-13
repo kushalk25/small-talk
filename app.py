10b28f01de9be3c819895c9a97065d979ceb36a3
Kushal Kumarasinghe <kushal.k@hotmail.com>
2023-09-13 18:51:44 -0400

    adding basic chatbox functionality

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Create a Flask application
 app = Flask(__name__)
 
-# Define a route and a view function
-@app.route('/')
-def hello_world():
-    return 'Hello, World!!!'
+# Initialize an empty conversation list to store messages
+conversation = []
+
+# Define a route for the home page
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Get the user's input from the form
+        user_input = request.form['user_input']
+
+        # Capitalize the user's input
+        capitalized_text = user_input.upper()
+
+        # Add user's input and capitalized response to the conversation
+        conversation.append({'user': user_input, 'bot': capitalized_text})
+
+    return render_template('index.html', conversation=conversation)
 
 # Run the application
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
