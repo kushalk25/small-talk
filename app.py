c3413d1e676d64a64e4ef38c488da078ea967c57
Kushal Kumarasinghe <kushal.k@hotmail.com>
2023-09-16 19:39:59 -0400

    adding chatpgt functionality along with requirements.txt and gitignore file

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
+import openai
 from flask import Flask, render_template, request
 
 # Create a Flask application
 app = Flask(__name__)
+
+# openai setup
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Initialize an empty conversation list to store messages
 conversation = []
@@ -13,13 +18,21 @@ def home():
         # Get the user's input from the form
         user_input = request.form['user_input']
 
-        # Capitalize the user's input
-        capitalized_text = user_input.upper()
+        chat_gpt_response = call_chat_gpt(user_input)
 
         # Add user's input and capitalized response to the conversation
-        conversation.append({'user': user_input, 'bot': capitalized_text})
+        conversation.append({'user': user_input, 'bot': chat_gpt_response})
 
     return render_template('index.html', conversation=conversation)
+
+def call_chat_gpt(input):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=input,
+        temperature=0.6,
+    )
+    return response.choices[0].text
+
 
 # Run the application
 if __name__ == '__main__':
